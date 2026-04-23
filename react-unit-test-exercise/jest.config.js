d130a1a126c777502c340969ce45bcd9dbd66f2d
TuanMc <maichungtuan@gmail.com>
2026-04-24 02:08:57 +0700

    Update unit test exercise

---
+++ react-unit-test-exercise/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
