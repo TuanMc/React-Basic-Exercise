2cf83d853eb90dcea32546c2cbee0132f462c8f6
TuanMc <maichungtuan@gmail.com>
2026-04-24 09:45:13 +0700

    Add coverage

--- react-unit-test-exercise/jest.config.js
+++ react-unit-test-exercise/jest.config.js
@@ -8,4 +8,24 @@ export default {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/**/*.spec.{js,jsx}',
+    '!src/main.jsx',
+    '!src/main-with-redux.jsx',
+    '!src/index.js',
+    '!src/tests/**',
+  ],
+  coverageReporters: ['text', 'lcov', 'html', 'json', 'json-summary'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 };
